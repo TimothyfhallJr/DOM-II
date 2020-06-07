dff9e56ccd239d3a9447ef29ea04390a827950ed
rufis1982 <rufii1982@hotmail.com>
2020-06-07 07:13:36 -0400

    some listeners done

--- js/index.js
+++ js/index.js
@@ -1,1 +1,26 @@
-// Your code goes here
+// Fun Bus Image
+const busPic = document.querySelector( 'img');
+
+audioObj = new Audio('media/busSound.wav');
+busPic.addEventListener("click", e => {
+   audioObj.play();
+});
+
+busPic.addEventListener('dblclick', e  => {
+  audioObj.pause();
+});
+
+// body  background Change 
+const body = document.querySelector('body');
+
+body.addEventListener("auxclick", e => {
+  const curColour = document.body.style.backgroundColor;
+
+  if (curColour === 'white') {
+      document.body.style.backgroundColor = "seagreen";
+  }
+  else {
+      document.body.style.backgroundColor = "white";
+  }
+});
+
